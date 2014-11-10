20c8849ccf765fd5220ee43ee4cdf72e85f12555
Shavit Netzer <shavit.netzer@gmail.com>
2014-11-10 18:37:47 +0200

    adding all examples

---
+++ app.js
@@ -0,0 +1,4 @@
+// blah 
+setTimeout(function () {
+	console.log("I've done my work!"); }, 2000);
+console.log("I'm waiting for all my work to finish.");
---
+++ test.js
@@ -0,0 +1,6 @@
+var http = require("http");
+var server = http.createServer(function(request, response) { response.writeHead(200, {"Content-Type": "text/plain"}); response.write("Hello World");
+response.end();
+});
+server.listen(8080)
+console.log('Listening on port 80.....');
